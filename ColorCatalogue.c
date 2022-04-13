157f3b61eaec6884ab140943a3b40e9b6b53f564
AbhinandanBajpayee <100206645+AbhinandanBajpayee@users.noreply.github.com>
2022-04-13 10:38:37 +0530

    Create ColorCatalogue.c

---
+++ ColorCatalogue.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <assert.h>
+#include "Color.h
+
+
+void ColorCatalogue()
+{
+  int colorCode = 1;
+  int lMajorColorIdx;
+  int lMinorColorIdx;
+  
+  printf(" MajorColor         MinorColor        ColorCode  \n");
+  
+  for( lMajorColorIdx = 0; lMajorColorIdx < numberOfMajorColors; lMajorColorIdx++ )
+  {
+     for( lMinorColorIdx = 0; lMinorColorIdx < numberOfMajorColors; lMinorColorIdx++ )
+     {
+         printf( "%s\t %s\t %d", MajorColorNames[lMajorColorIdx], MinorColorNames[lMinorColorIdx], colorCode );
+         colorCode++;
+     }
+  }
+}
