1412bb4223c766667d59a121e67d1eec70adf0ae
AbhinandanBajpayee <100206645+AbhinandanBajpayee@users.noreply.github.com>
2022-04-13 09:29:56 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,19 +2,6 @@
 #include <assert.h>
 #include "Color.h"
 
-void testPairToNumber(
-    enum MajorColor major,
-    enum MinorColor minor,
-    int expectedPairNumber)
-{
-    ColorPair colorPair;
-    colorPair.majorColor = major;
-    colorPair.minorColor = minor;
-    int pairNumber = GetPairNumberFromColor(&colorPair);
-    printf("Got pair number %d\n", pairNumber);
-    assert(pairNumber == expectedPairNumber);
-}
-
 int main() {
     testNumberToPair(4, WHITE, BROWN);
     testNumberToPair(5, WHITE, SLATE);
